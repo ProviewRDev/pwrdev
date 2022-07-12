ba30fe04f09a817a3a8367f69f956df386ec09ef
Marcus Nordenberg <marcus.nordenberg@ssab.com>
2022-07-12 22:15:10 +0200

    PROFINET: remove old parser

--- profibus/lib/rt/src/rt_pn_gsdml_data.h
+++ profibus/lib/rt/src/rt_pn_gsdml_data.h
@@ -48,7 +48,6 @@
 #include "co_xml_parser.h"
 #include "co_pugixml.hpp"
 #include "rt_profinet.h"
-//#include "rt_io_pnak_locals.h"
 
 #define RUNTIME_PARSER_STRING_MAX_LENGTH sizeof(pwr_tString256)
 #define DAP_SLOT 0
@@ -65,7 +64,6 @@ public:
   ProfinetDataRecord(ProfinetDataRecord&&);
   ProfinetDataRecord(ProfinetDataRecord const&) = default;
   ~ProfinetDataRecord();
-  // ProfinetDataRecord& operator=(ProfinetDataRecord const&);
   void build(pugi::xml_node&&) const;
 
   unsigned char* m_data;
@@ -73,33 +71,6 @@ public:
   unsigned short m_index;
   unsigned short m_transfer_sequence;
 };
-// class GsdmlDataRecord
-// {
-// public:
-//   GsdmlDataRecord()
-//       : record_idx(0), data(0), data_reversed_endianess(0), data_length(0),
-//         index(0), transfer_sequence(0)
-//   {
-//   }
-
-//   unsigned int record_idx;
-//   unsigned char* data;
-//   unsigned char* data_reversed_endianess;
-//   unsigned int data_length;
-//   unsigned short index;
-//   unsigned short transfer_sequence;
-
-//   ~GsdmlDataRecord()
-//   {
-//     if (data)
-//       free(data);
-//     if (data_reversed_endianess)
-//       free(data_reversed_endianess);
-//   }
-//   GsdmlDataRecord(const GsdmlDataRecord& x);
-
-//   int print(std::ofstream& fp, bool reverse_endianess);
-// };
 
 class ProfinetIOCR
 {
@@ -125,26 +96,8 @@ public:
   unsigned char* m_rt_io_data;
   unsigned short m_rt_clean_io_data_length;
   unsigned char* m_rt_clean_io_data;
-  uint m_rt_properties = 0; // Set TO PROFINET_IO_CR_RT_CLASS2 (hard coded for iocr in rt_pn_iface.cpp: ~673)
+  uint m_rt_properties = 0;
 };
-
-// class GsdmlIOCRData
-// {
-// public:
-//   GsdmlIOCRData()
-//       : type(0), properties(0), send_clock_factor(0), phase(0), api(0)
-//   {
-//   }
-
-//   unsigned short type;
-//   unsigned int properties;
-//   unsigned short send_clock_factor;
-//   unsigned short reduction_ratio;
-//   unsigned int phase;
-//   unsigned int api;
-
-//   int print(std::ofstream& fp);
-// };
 
 class ProfinetSubslot
 {
@@ -162,7 +115,7 @@ public:
   unsigned int m_submodule_ident_number;
   unsigned int m_io_input_length;
   unsigned int m_io_output_length;
-  unsigned int m_api; // Will default to 0. What's the correct default? TODO check specification
+  unsigned int m_api = PROFINET_DEFAULT_API ; // Will default to 0.
   std::string m_submodule_ID;
 
   // Elements
@@ -181,41 +134,6 @@ public:
   ushort m_rt_offset_status_in;
   ushort m_rt_offset_status_out;
 };
-
-// class GsdmlSubslotData
-// {
-// public:
-//   GsdmlSubslotData()
-//       : subslot_number(0), subslot_idx(0), submodule_enum_number(0),
-//         submodule_ident_number(0), api(0), io_input_length(0),
-//         io_output_length(0)
-//   {
-//   }
-
-//   std::vector<GsdmlDataRecord*> data_record;
-//   unsigned int subslot_number;
-//   unsigned int subslot_idx;
-//   unsigned int submodule_enum_number;
-//   unsigned int submodule_ident_number;
-//   unsigned int api;
-//   unsigned int io_input_length;
-//   unsigned int io_output_length;
-
-//   ~GsdmlSubslotData()
-//   {
-//     for (unsigned int i = 0; i < data_record.size(); i++)
-//       delete data_record[i];
-//   }
-//   GsdmlSubslotData(const GsdmlSubslotData& x)
-//       : subslot_number(x.subslot_number), subslot_idx(x.subslot_idx)
-//   {
-//     for (unsigned int i = 0; i < x.data_record.size(); i++)
-//     {
-//       data_record.push_back(new GsdmlDataRecord(*x.data_record[i]));
-//     }
-//   }
-//   int print(std::ofstream& fp, bool reverse_endianess);
-// };
 
 class ProfinetSlot
 {
@@ -245,48 +163,6 @@ public:
   ushort m_rt_phys_ident_number;
 };
 
-// //  GsdmlSlotData()
-//       : module_enum_number(0), dap_fixed_slot(0), module_class(0),
-//         module_oid(pwr_cNOid), slot_number(0), slot_idx(0)
-//   {
-//     module_text[0] = 0;
-//   }
-//   unsigned int module_enum_number;
-//   unsigned int module_ident_number;
-
-//   unsigned int dap_fixed_slot;
-//   pwr_tCid module_class;
-//   pwr_tOid module_oid;
-//   char module_text[160];
-//   unsigned int slot_number;
-
-//   unsigned int slot_idx;
-//   std::vector<GsdmlSubslotData*> subslot_data;
-
-//   ~GsdmlSlotData()
-//   {
-//     for (unsigned int i = 0; i < subslot_data.size(); i++)
-//       delete subslot_data[i];
-//   }
-//   void slot_reset()
-//   {
-//     for (unsigned int i = 0; i < subslot_data.size(); i++)
-//       delete subslot_data[i];
-//     subslot_data.clear();
-//   }
-//   GsdmlSlotData(const GsdmlSlotData& x)
-//       : module_enum_number(x.module_enum_number), module_class(x.module_class),
-//         module_oid(pwr_cNObjid), slot_number(x.slot_number),
-//         slot_idx(x.slot_idx)
-//   {
-//     for (unsigned int i = 0; i < x.subslot_data.size(); i++)
-//     {
-//       subslot_data.push_back(new GsdmlSubslotData(*x.subslot_data[i]));
-//     }
-//   }
-//   int print(std::ofstream& fp, bool reverse_endianess);
-// };
-
 class ProfinetExtChannelDiag
 {
 public:
@@ -303,17 +179,6 @@ public:
   std::string m_help;
 };
 
-// class GsdmlExtChannelDiag
-// {
-// public:
-//   unsigned short error_type;
-//   char name[200];
-//   char help[4096];
-//   // std::vector<GsdmlExtChannelAddValueDataItem*> data_item; TODO Do we want this extra level of
-//   diagnostic information available?
-
-//   int print(std::ofstream& fp);
-// };
 class ProfinetChannelDiag
 {
 public:
@@ -332,19 +197,6 @@ public:
   // Elements
   std::unordered_map<uint, ProfinetExtChannelDiag> m_ext_channel_diag_map;
 };
-
-// class GsdmlChannelDiag
-// {
-// public:
-//   GsdmlChannelDiag();
-//   unsigned short error_type;
-//   char name[200];
-//   char help[4096]; // We need a large buffer for most of the help text in the
-//                    // diagnostics...
-//   std::vector<GsdmlExtChannelDiag*> ext_channel_diag;
-
-//   int print(std::ofstream& fp);
-// };
 
 class ProfinetNetworkSettings
 {
@@ -395,8 +247,6 @@ public:
   unsigned short m_device_id;    // Part of DeviceIdentity
   unsigned int m_instance;       // Used to make out the ObjectUUID for DCERPC together with vendor ID and device ID.
 
-  // std::shared_ptr<PnDeviceData> m_pn_local_runtime_data;
-
   // Elements
   ProfinetNetworkSettings m_NetworkSettings;
   std::vector<ProfinetSlot> m_slot_list;
@@ -438,92 +288,6 @@ public:
 
   // Misc for configurator
   static ProfinetSlot* m_paste_slotdata;
-
-  // TODO Do we need this?
-  // int m_byte_order;
-  // int m_read_data_is_native_ordered;
-  // unsigned short m_instance; TODO Needed??
 };
-
-// class GsdmlDeviceData
-// {
-// public:
-//   GsdmlDeviceData()
-//       : device_num(0), skip_ip_assignment(0), vendor_id(0), device_id(0),
-//         byte_order(0), read_data_is_native_ordered(1), instance(0)
-//   {
-//     device_name[0] = 0;
-//     ip_address[0] = 0;
-//     subnet_mask[0] = 0;
-//     mac_address[0] = 0;
-//     device_text[0] = 0;
-//     version[0] = 0;
-//     gsdmlfile[0] = 0;
-//   }
-//   char device_name[80];
-//   char ip_address[20];
-//   char subnet_mask[20];
-//   char mac_address[20];
-//   int device_num;
-//   int skip_ip_assignment;
-//   char device_text[160];
-//   unsigned short vendor_id;
-//   unsigned short device_id;
-//   char version[20];
-//   int byte_order;
-//   int read_data_is_native_ordered;
-//   unsigned short instance;
-//   pwr_tFileName gsdmlfile;
-//   std::vector<GsdmlSlotData*> slot_data;
-//   std::vector<GsdmlIOCRData*> iocr_data;
-//   static GsdmlSlotData* paste_slotdata;
-//   std::vector<GsdmlChannelDiag*> channel_diag;
-
-//   ~GsdmlDeviceData()
-//   {
-//     device_reset();
-//     channel_diag_reset();
-//   }
-//   void device_reset()
-//   {
-//     for (unsigned int i = 0; i < slot_data.size(); i++)
-//       delete slot_data[i];
-//     slot_data.clear();
-//     for (unsigned int i = 0; i < iocr_data.size(); i++)
-//       delete iocr_data[i];
-//     iocr_data.clear();
-//   }
-//   void channel_diag_reset()
-//   {
-//     for (unsigned int i = 0; i < channel_diag.size(); i++)
-//       delete channel_diag[i];
-//     channel_diag.clear();
-//   }
-//   int print(const char* filename);
-//   int read(const char* filename, int new_filename = 0);
-//   int copy_slot(unsigned int slot_idx);
-//   int cut_slot(unsigned int slot_idx);
-//   int paste_slot(unsigned int slot_idx);
-//   int modify_value(const char* attr, const char* value);
-//   int get_value(const char* attr, char* buf, int bufsize);
-// };
-
-// class GsdmlDataReader : public co_xml_interpreter
-// {
-// public:
-//   GsdmlDataReader(GsdmlDeviceData* d) : data(d), new_filename(0) {}
-//   int tag(const char* name);
-//   int metatag(const char* name);
-//   int tag_end(const char* name);
-//   int metatag_end(const char* name);
-//   int tag_value(const char* value);
-//   int tag_attribute(const char* name, const char* value);
-
-//   int tag_name_to_id(const char* name, unsigned int* id);
-
-//   unsigned int current_tag;
-//   GsdmlDeviceData* data;
-//   int new_filename;
-// };
 
 #endif
