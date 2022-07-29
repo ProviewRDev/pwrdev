e9c07929230f0327ea4d0d49115a29993ed30f8f
Claes Sjöfors <claes.sjofors@ssab.com>
2022-07-29 16:51:26 +0200

    Build fixes for 6.0

--- wb/exp/pwrwb/src/setup_pwrwb.py
+++ wb/exp/pwrwb/src/setup_pwrwb.py
@@ -7,9 +7,9 @@ pwr_lib = os.environ['pwr_elib']
 pwr_obj = os.environ['pwr_eobj']
 pwr_inc = os.environ['pwr_einc']
 try:
-    pwre_conf_qt = os.environ['PWRE_CONF_QT']
+    pwre_conf_gtk = os.environ['PWRE_CONF_GTK']
 except:
-    pwre_conf_qt = ""
+    pwre_conf_gtk = ""
 
 pwre_conf_libos = os.environ['pwre_conf_lib'][1:].split(' ')
 pwre_conf_lib = []
@@ -21,7 +21,16 @@ pwre_conf_libwbos = os.environ['pwre_conf_libwb'][1:].split(' ')
 for lib in pwre_conf_libwbos:
     pwre_conf_libwb.append(lib[2:])    
 
-if pwre_conf_qt == "1":
+if pwre_conf_gtk == "1":
+    libs = ['pwr_wb_gtk', 'pwr_xtt_gtk', 'pwr_ge_gtk', 'pwr_cow_gtk',
+            'pwr_flow_gtk', 'pwr_glow_gtk',
+            'pwr_wb', 'pwr_xtt', 'pwr_ge', 'pwr_cow', 'pwr_flow', 'pwr_glow',
+            'pwr_wb_gtk', 'pwr_xtt_gtk', 'pwr_ge_gtk', 'pwr_cow_gtk', 'pwr_flow_gtk', 'pwr_glow_gtk',
+            'pwr_wb', 'pwr_xtt', 'pwr_ge', 'pwr_cow',
+            'pwr_flow', 'pwr_glow', 
+            'pwr_rt', 'pwr_statussrv', 'pwr_co', 'pwr_msg_dummy']
+    xlibs = ['gtk-3']
+else:
     libs = ['pwr_wb_qt', 'pwr_xtt_qt', 'pwr_ge_qt', 'pwr_cow_qt',
             'pwr_flow_qt', 'pwr_glow_qt',
             'pwr_wb', 'pwr_xtt', 'pwr_ge', 'pwr_cow', 'pwr_flow', 'pwr_glow',
@@ -30,15 +39,6 @@ if pwre_conf_qt == "1":
             'pwr_flow', 'pwr_glow', 
             'pwr_rt', 'pwr_statussrv', 'pwr_co', 'pwr_msg_dummy']
     xlibs = ['QtCore', 'QtGui']
-else:
-    libs = ['pwr_wb_gtk', 'pwr_xtt_gtk', 'pwr_ge_gtk', 'pwr_cow_gtk',
-            'pwr_flow_gtk', 'pwr_glow_gtk',
-            'pwr_wb', 'pwr_xtt', 'pwr_ge', 'pwr_cow', 'pwr_flow', 'pwr_glow',
-            'pwr_wb_gtk', 'pwr_xtt_gtk', 'pwr_ge_gtk', 'pwr_cow_gtk', 'pwr_flow_gtk', 'pwr_glow_gtk',
-            'pwr_wb', 'pwr_xtt', 'pwr_ge', 'pwr_cow',
-            'pwr_flow', 'pwr_glow', 
-            'pwr_rt', 'pwr_statussrv', 'pwr_co', 'pwr_msg_dummy']
-    xlibs = ['gtk-x11-2.0']
 pwrwbmodule = Extension( name='pwrwb',
                          sources=['pwrwbmodule.cpp'],
                          define_macros=[('OS_POSIX', '1'),
@@ -55,7 +55,7 @@ pwrwbmodule = Extension( name='pwrwb',
                          library_dirs=[pwr_lib],
                          libraries=libs +
                          pwre_conf_lib + pwre_conf_libwb + xlibs,
-#                        extra_link_args=['-L/usr/lib/x86_64-linux-gnu', subprocess.getoutput('pkg-config --libs gtk+-2.0')],
+#                        extra_link_args=['-L/usr/lib/x86_64-linux-gnu', subprocess.getoutput('pkg-config --libs gtk+-3.0')],
                          language='c++'
                        )
                       
