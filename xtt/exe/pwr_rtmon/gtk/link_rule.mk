ae8ace75727430e1678fedbb34754aafa90e0f76
Claes Sjöfors <claes.sjofors@ssab.com>
2023-06-28 14:17:10 +0200

    pwr_rtmon build fix

--- xtt/exe/pwr_rtmon/gtk/link_rule.mk
+++ xtt/exe/pwr_rtmon/gtk/link_rule.mk
@@ -5,6 +5,6 @@ link = $(ldxx) $(elinkflags) $(domap) -o $(pwr_exe)/pwr_rtmon_gtk \
 	$(bld_dir)/pwr_rtmon_gtk.o \
 	$(wb_msg_eobjs) $(rt_msg_eobjs) \
 	$(pwre_conf_libdir) $(pwre_conf_libpwrxttgtk) $(pwre_conf_libpwrxtt) $(pwre_conf_libgtk) \
-	$(pwre_conf_libpwrrt) $(pwre_conf_lib) $(pwre_conf_libpwrrt) -lrt
+	$(pwre_conf_libpwrrt) $(pwre_conf_lib) $(pwre_conf_libpwrrt) $(pwre_conf_lib) -lrt
 
 endif
