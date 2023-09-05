47dd2ef8a5c7ddb6743ae83f1e41865a7cc0beae
Claes Sjöfors <claes.sjofors@ssab.com>
2023-09-05 13:55:38 +0200

    Ge scroll event added

--- xtt/lib/glow/gtk/glow_growwidget_gtk.cpp
+++ xtt/lib/glow/gtk/glow_growwidget_gtk.cpp
@@ -359,7 +359,7 @@ static void growwidgetgtk_realize(GtkWidget* widget)
       | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK | GDK_KEY_PRESS_MASK
       | GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK
       | GDK_BUTTON_MOTION_MASK | GDK_ENTER_NOTIFY_MASK | GDK_LEAVE_NOTIFY_MASK
-      | GDK_STRUCTURE_MASK;
+      | GDK_STRUCTURE_MASK | GDK_SCROLL_MASK;
   attr.visual = gtk_widget_get_visual(widget);
 
   attr_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
