da26532edc6dc4bc33e277b9fbfd2dcb95d29166
Marcus Nordenberg <marcus.nordenberg@ssab.com>
2024-04-22 14:54:10 +0200

    simplify the dir_symbols.mk makefile
    And also add comments

--- src/tools/pwre/src/dir_symbols.mk
+++ src/tools/pwre/src/dir_symbols.mk
@@ -1,60 +1,20 @@
 #  dir_symbols.mk  -  get directory symbols
 
-comma := ,
-empty :=
-space := $(empty) $(empty)
+# This makefile sets some variables derived from the folder path structure:
+# comp_name = Component name
+# type_name = Type name
+# root_name = root name
 
-pred_13	:= 12
-pred_12	:= 11
-pred_11	:= 10
-pred_10	:= 9
-pred_9	:= 8
-pred_8	:= 7
-pred_7	:= 6
-pred_6	:= 5
-pred_5	:= 4
-pred_4	:= 3
-pred_3	:= 2
-pred_2	:= 1
-pred_1	:= 0
+src_name	:= $(notdir $(PWD))
+PARENT_DIR := $(patsubst %/,%,$(dir $(PWD))) 			# Fetch the parent directory of the current working directory
+comp_name := $(notdir $(PARENT_DIR))					# Extract the component name
+PARENT_DIR := $(patsubst %/,%,$(dir $(PARENT_DIR)))		# Fetch the parent directory once again from where PARENT_DIR currently is
+type_name := $(notdir $(PARENT_DIR))					# Extract the type name
+PARENT_DIR := $(patsubst %/,%,$(dir $(PARENT_DIR)))		# Fetch the parent directory once again from where PARENT_DIR currently is
+root_name := $(notdir $(PARENT_DIR))					# Extract the root name
 
-cwd_dirs := $(subst /,$(space),$(PWD))
-#cwd_dirs := $(subst .hw_,hw_,$(cwd_dirs))
-#cwd_dirs := $(subst .os_,os_,$(cwd_dirs))
-
-word	:= $(words $(cwd_dirs))
-#hw_name	:= $(word $(word), $(cwd_dirs))
-#ifneq ($(findstring hw, $(hw_name)),hw)
-#include ___error___hw
-#endif
-
-#prev	:= pred_ $(word)
-#pred	:= $(subst $(space),$(empty),$(prev))
-#word	:= $($(pred))
-#os_name	:= $(word $(word), $(cwd_dirs))
-#ifneq ($(findstring os,$(os_name)),os)
-#include ___error___os
-#endif
-
-#prev	:= pred_ $(word)
-#pred	:= $(subst $(space),$(empty),$(prev))
-#word	:= $($(pred))
-src_name	:= $(word $(word), $(cwd_dirs))
-#ifneq ($(src_name),src)
-#include ___error___no_source_subdirectory
-#endif
-
-prev	:= pred_ $(word)
-pred	:= $(subst $(space),$(empty),$(prev))
-word	:= $($(pred))
-comp_name	:= $(word $(word), $(cwd_dirs))
-
-prev	:= pred_ $(word)
-pred	:= $(subst $(space),$(empty),$(prev))
-word	:= $($(pred))
-type_name	:= $(word $(word), $(cwd_dirs))
-
-prev	:= pred_ $(word)
-pred	:= $(subst $(space),$(empty),$(prev))
-word	:= $($(pred))
-root_name	:= $(word $(word), $(cwd_dirs))
+# Clean the variables from any whitespaces, tabs, newlines or carrige return
+src_name := $(shell echo "$(src_name)" | tr -d ' \t\r\n')
+comp_name := $(shell echo "$(comp_name)" | tr -d ' \t\r\n')
+type_name := $(shell echo "$(type_name)" | tr -d ' \t\r\n')
+root_name := $(shell echo "$(root_name)" | tr -d ' \t\r\n')
