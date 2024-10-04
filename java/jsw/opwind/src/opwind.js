81c58829b0301f8bd52c061a22a30333657eec65
Daniel Claeson <daniel.claeson@ssab.com>
2024-10-04 10:38:58 +0200

    Format source code in java/jsw

--- java/jsw/opwind/src/opwind.js
+++ java/jsw/opwind/src/opwind.js
@@ -1,10 +1,8 @@
 "use strict";
 
-#jsc_include crypt.jsi
-#jsc_include pwr.jsi
-#jsc_include gdh.jsi
-
-
+// #jsc_include crypt.jsi
+// #jsc_include pwr.jsi
+// #jsc_include gdh.jsi
 
 /** Start OpWind **/
 
@@ -16,267 +14,288 @@ function OpWindMenu() {
   this.user_text = null;
   this.host;
 
-  var self = this;  
+  var self = this;
 
-  this.init = function() {
+  this.init = function () {
     this.host = window.location.hostname;
-    if ( this.host == "")
-      this.host = "localhost";
+    if (this.host == "") this.host = "localhost";
 
     this.gdh = new Gdh();
     this.gdh.open_cb = this.gdh_init_cb;
     this.gdh.init();
   };
 
-  this.is_authorized = function( access) {
-    return ( this.priv & access) ? true : false;
-  }
+  this.is_authorized = function (access) {
+    return this.priv & access ? true : false;
+  };
 
-  this.get_opplace = function() {
+  this.get_opplace = function () {
     var query = window.location.search.substring(1);
 
     console.log("query", query);
-    var vars = query.split('&');
+    var vars = query.split("&");
     console.log("vars", vars.length, vars[0].substring(8));
     return vars[0].substring(8);
   };
 
-  this.gdh_init_cb = function() {
-    var oid = new PwrtObjid( 0, 0);
+  this.gdh_init_cb = function () {
+    var oid = new PwrtObjid(0, 0);
     //self.gdh.getSocketServerVersion(self.get_version_cb, self);
     self.user = "Default";
-    self.gdh.login( "", "", self.login_cb, self);
-    self.gdh.getOpwindMenu( self.get_opplace(), self.get_menu_cb, 999);
-    
+    self.gdh.login("", "", self.login_cb, self);
+    self.gdh.getOpwindMenu(self.get_opplace(), self.get_menu_cb, 999);
   };
 
-  this.add_menu_button = function( context, text) {
-      var button = document.createElement( "input");
-      button.type = "button";
-      button.className = "leftmenu-button";
-      button.value = text;
-      button.addEventListener( 'click', new Function( "menu.button_cb(\"" + button.value + "\")"));
-      context.appendChild( button);
-      context.appendChild( document.createElement( "br"));
-      return button;
+  this.add_menu_button = function (context, text) {
+    var button = document.createElement("input");
+    button.type = "button";
+    button.className = "leftmenu-button";
+    button.value = text;
+    button.addEventListener(
+      "click",
+      new Function('menu.button_cb("' + button.value + '")'),
+    );
+    context.appendChild(button);
+    context.appendChild(document.createElement("br"));
+    return button;
   };
 
-  this.get_menu_cb = function( id, data, sts, result) {
+  this.get_menu_cb = function (id, data, sts, result) {
     console.log("get menu version", self.gdh.serverVersion());
     self.info = result;
-    console.log( "Menu received", sts, data, result.buttons.length);
+    console.log("Menu received", sts, data, result.buttons.length);
     var context = document.getElementById("opwindmenu");
 
     document.getElementById("opwind_title").innerHTML = result.title;
     document.getElementById("opwind_text").innerHTML = result.text;
 
-    if ( result.enable_login) {
-      self.user_text = document.createTextNode( self.user + " on " + self.host);
-      context.appendChild( self.user_text);
-      context.appendChild( document.createElement( "hr"));
+    if (result.enable_login) {
+      self.user_text = document.createTextNode(self.user + " on " + self.host);
+      context.appendChild(self.user_text);
+      context.appendChild(document.createElement("hr"));
 
-      document.getElementById("login_button").addEventListener( "click", function( event) {
-           if ( document.getElementById("login_frame").style.visibility == 'hidden') {
-             document.getElementById("login_user").value = "";
-             document.getElementById("login_passw").value = "";
-             document.getElementById("login_frame").style.visibility='visible';
-             document.getElementById("login_frame").style.height='120px';
-	     document.getElementById("login_user").focus();
-           }
-           else {
-             document.getElementById("login_frame").style.visibility='hidden';
-             document.getElementById("login_frame").style.height='0px';
-           }
-      });
-      document.getElementById("apply_button").addEventListener( "click", function( event) {
-           var user = document.getElementById("login_user").value;
-           var passwd = document.getElementById("login_passw").value;
-	   if ( user.trim() == "")
-	     return;
-           document.getElementById("login_frame").style.visibility='hidden';
-           document.getElementById("login_frame").style.height='0px';
-           var c = new JopCrypt();
-           passwd = c.crypt( "aa", passwd);
+      document
+        .getElementById("login_button")
+        .addEventListener("click", function (event) {
+          if (
+            document.getElementById("login_frame").style.visibility == "hidden"
+          ) {
+            document.getElementById("login_user").value = "";
+            document.getElementById("login_passw").value = "";
+            document.getElementById("login_frame").style.visibility = "visible";
+            document.getElementById("login_frame").style.height = "120px";
+            document.getElementById("login_user").focus();
+          } else {
+            document.getElementById("login_frame").style.visibility = "hidden";
+            document.getElementById("login_frame").style.height = "0px";
+          }
+        });
+      document
+        .getElementById("apply_button")
+        .addEventListener("click", function (event) {
+          var user = document.getElementById("login_user").value;
+          var passwd = document.getElementById("login_passw").value;
+          if (user.trim() == "") return;
+          document.getElementById("login_frame").style.visibility = "hidden";
+          document.getElementById("login_frame").style.height = "0px";
+          var c = new JopCrypt();
+          passwd = c.crypt("aa", passwd);
 
-           self.user = user;
-           self.gdh.login( user, passwd, self.login_cb, self);
-      });
-      document.getElementById("cancel_button").addEventListener( "click", function( event) {
-	   document.getElementById("login_frame").style.visibility='hidden';
-           document.getElementById("login_frame").style.height='0px';
-      });
-      document.getElementById("logout_button").addEventListener( "click", function( event) {
-           document.getElementById("login_frame").style.visibility='hidden';
-           document.getElementById("login_frame").style.height='0px';
-	   self.priv = 0;
-	   self.user = "Default";
-	   self.gdh.login( "", "", self.login_cb, self);
-      });
+          self.user = user;
+          self.gdh.login(user, passwd, self.login_cb, self);
+        });
+      document
+        .getElementById("cancel_button")
+        .addEventListener("click", function (event) {
+          document.getElementById("login_frame").style.visibility = "hidden";
+          document.getElementById("login_frame").style.height = "0px";
+        });
+      document
+        .getElementById("logout_button")
+        .addEventListener("click", function (event) {
+          document.getElementById("login_frame").style.visibility = "hidden";
+          document.getElementById("login_frame").style.height = "0px";
+          self.priv = 0;
+          self.user = "Default";
+          self.gdh.login("", "", self.login_cb, self);
+        });
 
       document.getElementById("login_user").value = "";
       document.getElementById("login_passw").value = "";
       //document.getElementById("login_frame").setAttribute("style", "visibility:hidden;height:10px";
-      document.getElementById("login_frame").style.visibility='hidden';
-      document.getElementById("login_frame").style.height='0px';
-    }
-    else {
+      document.getElementById("login_frame").style.visibility = "hidden";
+      document.getElementById("login_frame").style.height = "0px";
+    } else {
       document.getElementById("login_button").remove();
       document.getElementById("login_frame").remove();
     }
-    if ( result.enable_language)
-      self.add_menu_button( context, "Language");
-    if ( result.enable_alarmlist) {
-      self.add_menu_button( context, "AlarmList");
-      self.add_menu_button( context, "EventList");
+    if (result.enable_language) self.add_menu_button(context, "Language");
+    if (result.enable_alarmlist) {
+      self.add_menu_button(context, "AlarmList");
+      self.add_menu_button(context, "EventList");
     }
-    if ( result.enable_eventlog)
-      self.add_menu_button( context, "EventLog");
-    if ( result.enable_navigator)
-      self.add_menu_button( context, "Navigator");
-    if ( !result.disable_help)
-      self.add_menu_button( context, "Help");
-    if ( !result.disable_proview)
-      self.add_menu_button( context, "ProviewR");
-    
-    context.appendChild( document.createElement( "hr"));
+    if (result.enable_eventlog) self.add_menu_button(context, "EventLog");
+    if (result.enable_navigator) self.add_menu_button(context, "Navigator");
+    if (!result.disable_help) self.add_menu_button(context, "Help");
+    if (!result.disable_proview) self.add_menu_button(context, "ProviewR");
 
+    context.appendChild(document.createElement("hr"));
 
     var button;
-    for ( var i = 0; i < result.buttons.length; i++) {
-      self.add_menu_button( context, result.buttons[i].text);
+    for (var i = 0; i < result.buttons.length; i++) {
+      self.add_menu_button(context, result.buttons[i].text);
     }
   };
 
-  this.button_cb = function( text) {
-
-    if ( self.info.enable_language && text == "Language") {
+  this.button_cb = function (text) {
+    if (self.info.enable_language && text == "Language") {
       console.log("Language activated");
-    }
-    else if ( self.info.enable_alarmlist && text == "AlarmList") {
+    } else if (self.info.enable_alarmlist && text == "AlarmList") {
       console.log("AlarmList activated");
-      if ( !(self.is_authorized( Pwr.mAccess_RtRead | Pwr.mAccess_RtWrite | 
-				 Pwr.mAccess_AllOperators |
-				 Pwr.mAccess_System | Pwr.mAccess_Maintenance |
-				 Pwr.mAccess_Process | Pwr.mAccess_Instrument)))
-	window.alert("Not authorized for this operation");
-      else
-	window.open( "ev.html?list=alarm", "_blank");
-    }
-    else if ( self.info.enable_alarmlist && text == "EventList") {
+      if (
+        !self.is_authorized(
+          Pwr.mAccess_RtRead |
+            Pwr.mAccess_RtWrite |
+            Pwr.mAccess_AllOperators |
+            Pwr.mAccess_System |
+            Pwr.mAccess_Maintenance |
+            Pwr.mAccess_Process |
+            Pwr.mAccess_Instrument,
+        )
+      )
+        window.alert("Not authorized for this operation");
+      else window.open("ev.html?list=alarm", "_blank");
+    } else if (self.info.enable_alarmlist && text == "EventList") {
       console.log("EventList activated");
-      if ( !(self.is_authorized( Pwr.mAccess_RtRead | Pwr.mAccess_RtWrite | 
-				 Pwr.mAccess_AllOperators |
-				 Pwr.mAccess_System | Pwr.mAccess_Maintenance |
-				 Pwr.mAccess_Process | Pwr.mAccess_Instrument)))
-	window.alert("Not authorized for this operation");
-      else
-	window.open( "ev.html?list=event", "_blank");
-    }
-    else if ( self.info.enable_eventlog && text == "EventLog") {
+      if (
+        !self.is_authorized(
+          Pwr.mAccess_RtRead |
+            Pwr.mAccess_RtWrite |
+            Pwr.mAccess_AllOperators |
+            Pwr.mAccess_System |
+            Pwr.mAccess_Maintenance |
+            Pwr.mAccess_Process |
+            Pwr.mAccess_Instrument,
+        )
+      )
+        window.alert("Not authorized for this operation");
+      else window.open("ev.html?list=event", "_blank");
+    } else if (self.info.enable_eventlog && text == "EventLog") {
       console.log("EventLog activated");
-      if ( !(self.is_authorized( Pwr.mAccess_RtRead | Pwr.mAccess_RtWrite | 
-				 Pwr.mAccess_AllOperators |
-				 Pwr.mAccess_System | Pwr.mAccess_Maintenance |
-				 Pwr.mAccess_Process | Pwr.mAccess_Instrument)))
-	   
-	window.alert("Not authorized for this operation");
-      else
-	window.alert("Not yet implemented");
-    }
-    else if ( self.info.enable_navigator && text == "Navigator") {
+      if (
+        !self.is_authorized(
+          Pwr.mAccess_RtRead |
+            Pwr.mAccess_RtWrite |
+            Pwr.mAccess_AllOperators |
+            Pwr.mAccess_System |
+            Pwr.mAccess_Maintenance |
+            Pwr.mAccess_Process |
+            Pwr.mAccess_Instrument,
+        )
+      )
+        window.alert("Not authorized for this operation");
+      else window.alert("Not yet implemented");
+    } else if (self.info.enable_navigator && text == "Navigator") {
       console.log("Navigator activated");
-      if ( !(self.is_authorized( Pwr.mAccess_RtNavigator | 
-				 Pwr.mAccess_System | Pwr.mAccess_Maintenance |
-				 Pwr.mAccess_Process | Pwr.mAccess_Instrument)))
-	window.alert("Not authorized for this operation");
-      else
-	window.open( "xtt.html", "_blank");
-    }
-    else if ( !self.info.disable_help && text == "Help") {
+      if (
+        !self.is_authorized(
+          Pwr.mAccess_RtNavigator |
+            Pwr.mAccess_System |
+            Pwr.mAccess_Maintenance |
+            Pwr.mAccess_Process |
+            Pwr.mAccess_Instrument,
+        )
+      )
+        window.alert("Not authorized for this operation");
+      else window.open("xtt.html", "_blank");
+    } else if (!self.info.disable_help && text == "Help") {
       console.log("Help activated");
-      window.open( "xtt_help_index.html", "_blank");
-    }
-    else if ( !self.info.disable_proview && text == "ProviewR") {
+      window.open("xtt_help_index.html", "_blank");
+    } else if (!self.info.disable_proview && text == "ProviewR") {
       console.log("ProviewR activated");
-      window.open( "http://www.proview.se", "_blank");
-    }
-    else {
-      if ( !(self.is_authorized( Pwr.mAccess_RtRead | Pwr.mAccess_RtWrite | 
-				 Pwr.mAccess_AllOperators |
-				 Pwr.mAccess_System | Pwr.mAccess_Maintenance |
-				 Pwr.mAccess_Process | Pwr.mAccess_Instrument)))
-	window.alert("Not authorized for this operation");
+      window.open("http://www.proview.se", "_blank");
+    } else {
+      if (
+        !self.is_authorized(
+          Pwr.mAccess_RtRead |
+            Pwr.mAccess_RtWrite |
+            Pwr.mAccess_AllOperators |
+            Pwr.mAccess_System |
+            Pwr.mAccess_Maintenance |
+            Pwr.mAccess_Process |
+            Pwr.mAccess_Instrument,
+        )
+      )
+        window.alert("Not authorized for this operation");
       else {
-	for ( var i = 0; i < self.info.buttons.length; i++) {
-	  if ( self.info.buttons[i].text == text) {
-	    console.log("Found", self.info.buttons[i].text);
-	    if (self.info.buttons[i].name != "") {
-	      var name = self.info.buttons[i].name;
-	      var n = name.indexOf(".pwg");
-	      if ( n != -1)
-		name = name.substring(0,n);
+        for (var i = 0; i < self.info.buttons.length; i++) {
+          if (self.info.buttons[i].text == text) {
+            console.log("Found", self.info.buttons[i].text);
+            if (self.info.buttons[i].name != "") {
+              var name = self.info.buttons[i].name;
+              var n = name.indexOf(".pwg");
+              if (n != -1) name = name.substring(0, n);
               var url;
-   	      if (self.info.buttons[i].object != null && self.info.buttons[i].object != "")
-	        url = "ge.html?graph=" + name + "&instance=" + self.info.buttons[i].object;
-              else
-	        url = "ge.html?graph=" + name;
-	    }
-	    else {
-	      var url = self.info.buttons[i].url;
-	      if (url.charAt(0) == '$') {
-		var idx = url.indexOf("/");
-		if (idx == -1)
-		  idx = url.length;
-		var sym = url.substring(1, idx);
-		for (var j = 0; j < 10; j++) {
-		  var symbols = self.info.url_symbols[j].split(" ");
-		  if (sym == symbols[0]) {
-		    url = symbols[1] + url.substring(idx);
-		    break;
-		  }
-		}
-	      }
-	    }
-	    console.log("url", url);
-	    window.open( url, "_blank");
-	    break;
-	  }
-	}
+              if (
+                self.info.buttons[i].object != null &&
+                self.info.buttons[i].object != ""
+              )
+                url =
+                  "ge.html?graph=" +
+                  name +
+                  "&instance=" +
+                  self.info.buttons[i].object;
+              else url = "ge.html?graph=" + name;
+            } else {
+              var url = self.info.buttons[i].url;
+              if (url.charAt(0) == "$") {
+                var idx = url.indexOf("/");
+                if (idx == -1) idx = url.length;
+                var sym = url.substring(1, idx);
+                for (var j = 0; j < 10; j++) {
+                  var symbols = self.info.url_symbols[j].split(" ");
+                  if (sym == symbols[0]) {
+                    url = symbols[1] + url.substring(idx);
+                    break;
+                  }
+                }
+              }
+            }
+            console.log("url", url);
+            window.open(url, "_blank");
+            break;
+          }
+        }
       }
     }
   };
 
-  this.get_version_cb = function( id, data, sts, result) {
-    if ( sts & 1)
-      console.log("Version", result);
-    else
-      console.log("Version unknown");
+  this.get_version_cb = function (id, data, sts, result) {
+    if (sts & 1) console.log("Version", result);
+    else console.log("Version unknown");
   };
 
-  this.login_cb = function( id, data, sts, result) {
-    console.log( "Login:", sts, result);
-    if ( sts & 1) {
+  this.login_cb = function (id, data, sts, result) {
+    console.log("Login:", sts, result);
+    if (sts & 1) {
       self.priv = result;
       sessionStorage.setItem("pwr_privilege", self.priv);
-      if ( self.user_text != null)
-	self.user_text.textContent = self.user + " on " + self.host;
-      
-      console.log( "Login", self.user, "Priv", self.priv);
-    }
-    else {
+      if (self.user_text != null)
+        self.user_text.textContent = self.user + " on " + self.host;
+
+      console.log("Login", self.user, "Priv", self.priv);
+    } else {
       self.priv = 0;
       self.user = "none";
       sessionStorage.setItem("pwr_privilege", self.priv);
-      if ( self.user_text != null)
-	self.user_text.textContent = "None on " + self.host;
-      console.log( "Login failure", "Priv", self.priv);
+      if (self.user_text != null)
+        self.user_text.textContent = "None on " + self.host;
+      console.log("Login failure", "Priv", self.priv);
     }
   };
 }
 
-
 var menu = new OpWindMenu();
 menu.init();
-
 
 /** End OpWind **/
