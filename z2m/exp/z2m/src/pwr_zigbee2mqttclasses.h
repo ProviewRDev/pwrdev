57caf281c4d347efc18ac76983de172e99341cf3
Claes Sjöfors <claes.sjofors@ssab.com>
2025-02-07 08:55:08 +0100

    z2m, fix in common structfile

--- z2m/exp/z2m/src/pwr_zigbee2mqttclasses.h
+++ z2m/exp/z2m/src/pwr_zigbee2mqttclasses.h
@@ -1,9 +1,14 @@
 #ifndef pwr_zigbee2mqttclasses_h
 #define pwr_zigbee2mqttclasses_h
 
-#include "pwr_zigbee2mqttclasses1.h"
-#include "pwr_zigbee2mqttclasses2.h"
-#include "pwr_zigbee2mqttclasses3.h"
-#include "pwr_zigbee2mqttclasses4.h"
+#include "pwr_zigbee2mqtt1classes.h"
+#include "pwr_zigbee2mqtt2classes.h"
+#include "pwr_zigbee2mqtt3classes.h"
+#include "pwr_zigbee2mqtt4classes.h"
+#include "pwr_zigbee2mqtt5classes.h"
+#include "pwr_zigbee2mqtt6classes.h"
+#include "pwr_zigbee2mqtt7classes.h"
+#include "pwr_zigbee2mqtt8classes.h"
+#include "pwr_zigbee2mqtt9classes.h"
 
 #endif
