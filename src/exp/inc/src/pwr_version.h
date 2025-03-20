97539a89dbefbd71023edc733c3f277f8b003e44
Claes Sjöfors <claes.sjofors@ssab.com>
2025-03-20 10:07:35 +0100

    Version set to 6.1.5

--- src/exp/inc/src/pwr_version.h
+++ src/exp/inc/src/pwr_version.h
@@ -76,10 +76,10 @@
   ((pwr_tVersion)((a << 24) + (b << 16) + (c << 8) + d))
 #endif
 
-#define pwrv_cBuildTimeStr "15-JAN-2025 12:00:00"
+#define pwrv_cBuildTimeStr "19-MAR-2025 12:00:00"
 
-#define pwrv_cPwrVersionStr "V6.1.4"
-#define pwrv_cPwrVersion SET_VERSION('V', 6, 1, 4)
+#define pwrv_cPwrVersionStr "V6.1.5"
+#define pwrv_cPwrVersion SET_VERSION('V', 6, 1, 5)
 
 #define pwrv_cWbdbVersionShortStr "V61"
 #define pwrv_cPwrCopyright "Copyright (C) 2005-2025 SSAB EMEA AB"
