493b649db4f3ab9b064004501b5de1756bdec408
Marcus Nordenberg <marcus.nordenberg@ssab.com>
2025-03-21 15:06:49 +0000

    Refactor csup_Exec to enhance delay handling logic and improve readability when logging errors

--- src/lib/rt/src/rt_csup.h
+++ src/lib/rt/src/rt_csup.h
@@ -41,7 +41,8 @@
 
 #include "rt_lst.h"
 
-typedef struct {
+typedef struct
+{
   lst_sEntry le;
   pwr_sClass_CycleSup* o;
   pwr_sAttrRef aref;
@@ -51,7 +52,7 @@ typedef struct {
 
 void csup_Exit(pwr_tStatus*, lst_sEntry*);
 lst_sEntry* csup_Init(pwr_tStatus*, pwr_tObjid, float);
-int csup_Exec(
-    pwr_tStatus*, lst_sEntry*, pwr_tDeltaTime*, pwr_tDeltaTime*, pwr_tTime*);
+int csup_Exec(pwr_tStatus* status, lst_sEntry* lh, pwr_tDeltaTime* next_start, pwr_tDeltaTime* stop,
+              pwr_tTime* now, pwr_tObjid* delayed_thread_objid);
 
 #endif
