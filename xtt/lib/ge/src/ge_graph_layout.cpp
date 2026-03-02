5388ac5beaf521ed94f4af96ca3045ca63720796
Marcus Nordenberg <marcus.nordenberg@ssab.com>
2026-03-02 15:52:30 +0100

    Format code

--- xtt/lib/ge/src/ge_graph_layout.cpp
+++ xtt/lib/ge/src/ge_graph_layout.cpp
@@ -51,36 +51,40 @@
 #define HORIZ 1
 #define MAX_PRIO 20
 
-typedef struct {
+typedef struct
+{
   int idx;
   double size;
   double coord;
 } sRowItem;
 
-typedef struct {
+typedef struct
+{
   double pref_size;
   double calc_size;
   double calc_coord;
   int fix_size;
 } sDirItem;
-  
-typedef struct {
+
+typedef struct
+{
   sDirItem dir[2];
   int disabled;
   int prio;
 } sItem;
 
-typedef struct {
+typedef struct
+{
   int msize;
   int row_cnt[2];
-  int *row_size[2];
-  sRowItem *row_idx[2];
+  int* row_size[2];
+  sRowItem* row_idx[2];
   double wind_size[2];
   double calc_wind_size[2];
-  sItem *item;
+  sItem* item;
 } sCtx;
 
-static int next(sCtx *ctx, int dir, int idx, int row, ccm_tInt *a_next, ccm_tInt *a_prev)
+static int next(sCtx* ctx, int dir, int idx, int row, ccm_tInt* a_next, ccm_tInt* a_prev)
 {
   int orig_row;
   int orig_row_size;
@@ -90,75 +94,89 @@ static int next(sCtx *ctx, int dir, int idx, int row, ccm_tInt *a_next, ccm_tInt
   ctx->row_size[dir][row]++;
   orig_row = row;
   orig_row_size = ctx->row_size[dir][row];
-  if (a_next[idx] == BORDER) {
+  if (a_next[idx] == BORDER)
+  {
     return 1;
-  } else {
+  }
+  else
+  {
     sts = next(ctx, dir, a_next[idx], row, a_next, a_prev);
     if (EVEN(sts))
       return sts;
   }
-  for (int j = 0; j < ctx->msize; j++) {
-    if (a_prev[j] == idx && j != a_next[idx]) {
-      //printf("New row %d, %d -> %d\n", ctx->row_cnt[dir], idx, j);
-      // New row, copy original row
+  for (int j = 0; j < ctx->msize; j++)
+  {
+    if (a_prev[j] == idx && j != a_next[idx])
+    {
+      // printf("New row %d, %d -> %d\n", ctx->row_cnt[dir], idx, j);
+      //  New row, copy original row
       if (ctx->row_cnt[dir] >= ROW_MAX - 1)
-	return 0;
+        return 0;
       for (int k = 0; k < orig_row_size; k++)
-	ctx->row_idx[dir][ctx->row_cnt[dir] * ctx->msize + k].idx = ctx->row_idx[dir][orig_row * ctx->msize + k].idx;
+        ctx->row_idx[dir][ctx->row_cnt[dir] * ctx->msize + k].idx =
+            ctx->row_idx[dir][orig_row * ctx->msize + k].idx;
       ctx->row_size[dir][ctx->row_cnt[dir]] = orig_row_size;
       ctx->row_cnt[dir]++;
       sts = next(ctx, dir, j, ctx->row_cnt[dir] - 1, a_next, a_prev);
       if (EVEN(sts))
-	return sts;
+        return sts;
     }
   }
   return 1;
 }
 
-static void disable(sCtx *ctx, int dir)
+static void disable(sCtx* ctx, int dir)
 {
   // Check if disabled
-  for (int i = 0; i < ctx->row_cnt[dir]; i++) {
+  for (int i = 0; i < ctx->row_cnt[dir]; i++)
+  {
     double total_size = 0;
-    for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-      if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
-	total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
+    for (int j = 0; j < ctx->row_size[dir][i]; j++)
+    {
+      if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
+        total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
     }
-    while (total_size > ctx->wind_size[dir]) {
+    while (total_size > ctx->wind_size[dir])
+    {
       int low_prio = 100;
       int low_prio_idx = -1;
 
-      // Disable item with lowest prio      
-      for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-	if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled &&
-	     ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].prio < low_prio) {
-	  low_prio_idx = ctx->row_idx[dir][i * ctx->msize + j].idx;
-	  low_prio = ctx->item[low_prio_idx].prio;
-	}
+      // Disable item with lowest prio
+      for (int j = 0; j < ctx->row_size[dir][i]; j++)
+      {
+        if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled &&
+            ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].prio < low_prio)
+        {
+          low_prio_idx = ctx->row_idx[dir][i * ctx->msize + j].idx;
+          low_prio = ctx->item[low_prio_idx].prio;
+        }
       }
       if (low_prio_idx != -1 && low_prio != MAX_PRIO)
-	ctx->item[low_prio_idx].disabled = 1;
+        ctx->item[low_prio_idx].disabled = 1;
 
       total_size = 0;
-      for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-	if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
-	  total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
+      for (int j = 0; j < ctx->row_size[dir][i]; j++)
+      {
+        if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
+          total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
       }
       if (low_prio == MAX_PRIO)
-	break;
+        break;
     }
   }
 }
 
-static void calculate(sCtx *ctx, int dir)
+static void calculate(sCtx* ctx, int dir)
 {
   // Calculate window size
   ctx->calc_wind_size[dir] = 0;
-  for (int i = 0; i < ctx->row_cnt[dir]; i++) {
+  for (int i = 0; i < ctx->row_cnt[dir]; i++)
+  {
     double total_size = 0;
-    for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-      if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
-	total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
+    for (int j = 0; j < ctx->row_size[dir][i]; j++)
+    {
+      if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
+        total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
     }
     if (total_size > ctx->calc_wind_size[dir])
       ctx->calc_wind_size[dir] = total_size;
@@ -166,33 +184,39 @@ static void calculate(sCtx *ctx, int dir)
   if (ctx->wind_size[dir] > ctx->calc_wind_size[dir])
     ctx->calc_wind_size[dir] = ctx->wind_size[dir];
 
-  for (int i = 0; i < ctx->row_cnt[dir]; i++) {
+  for (int i = 0; i < ctx->row_cnt[dir]; i++)
+  {
     double total_size = 0;
-    for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-      if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
-	total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
+    for (int j = 0; j < ctx->row_size[dir][i]; j++)
+    {
+      if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
+        total_size += ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
     }
 
-    if (total_size > 0) {
+    if (total_size > 0)
+    {
       // Spread space
       double add_size;
       int cnt = 0;
-      for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-	if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled &&
-	     !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].fix_size)
-	  cnt++;
+      for (int j = 0; j < ctx->row_size[dir][i]; j++)
+      {
+        if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled &&
+            !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].fix_size)
+          cnt++;
       }
       if (cnt > 0)
-	add_size = MAX(0,(ctx->calc_wind_size[dir] - total_size) / cnt);
-      for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-	if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled) {
-	  if (ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].fix_size)
-	    ctx->row_idx[dir][i * ctx->msize + j].size = 
-	      ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
-	  else
-	    ctx->row_idx[dir][i * ctx->msize + j].size = 
-	      ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size + add_size;
-	}
+        add_size = MAX(0, (ctx->calc_wind_size[dir] - total_size) / cnt);
+      for (int j = 0; j < ctx->row_size[dir][i]; j++)
+      {
+        if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
+        {
+          if (ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].fix_size)
+            ctx->row_idx[dir][i * ctx->msize + j].size =
+                ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size;
+          else
+            ctx->row_idx[dir][i * ctx->msize + j].size =
+                ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].pref_size + add_size;
+        }
       }
     }
   }
@@ -200,52 +224,68 @@ static void calculate(sCtx *ctx, int dir)
   // Adjust if size differs
   int adjusted = 1;
   int adjust_cnt = 0;
-  while (adjusted && adjust_cnt < 20) {
+  while (adjusted && adjust_cnt < 20)
+  {
     adjusted = 0;
     adjust_cnt++;
-    for (int k = 0; k < ctx->msize; k++) {
+    for (int k = 0; k < ctx->msize; k++)
+    {
       double last_size = 0;
       int last_rowitem;
-      if (!ctx->item[k].disabled) {
-	for (int i = 0; i < ctx->row_cnt[dir]; i++) {
-	  for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-	    if (ctx->row_idx[dir][i * ctx->msize + j].idx == k) {
-	      if (last_size == 0) {
-		last_rowitem = i * ctx->msize + j;
-		last_size = ctx->row_idx[dir][last_rowitem].size;
-	      } else {
-		if (!(fabs(last_size - ctx->row_idx[dir][i * ctx->msize + j].size) < DBL_EPSILON)) {
-		  if (j == ctx->row_size[dir][i] - 1 && j != 0) {
-		    // Adjust previous
-		    ctx->row_idx[dir][i * ctx->msize + j - 1].size -= last_size - 
-		        ctx->row_idx[dir][i * ctx->msize + j].size;
-		    ctx->row_idx[dir][i * ctx->msize + j].size = last_size;
-		  } else {
-		    // Adjust next
-		    ctx->row_idx[dir][i * ctx->msize + j + 1].size -= last_size - 
-		        ctx->row_idx[dir][i * ctx->msize + j].size;
-		    ctx->row_idx[dir][i * ctx->msize + j].size = last_size;
-		  }
-		  adjusted = 1;
-		}
-	      }
-	      if (ctx->item[k].dir[dir].calc_size < ctx->row_idx[dir][i * ctx->msize + j].size)
-		ctx->item[k].dir[dir].calc_size = ctx->row_idx[dir][i * ctx->msize + j].size;
-	    }
-	  }
-	}	
+      if (!ctx->item[k].disabled)
+      {
+        for (int i = 0; i < ctx->row_cnt[dir]; i++)
+        {
+          for (int j = 0; j < ctx->row_size[dir][i]; j++)
+          {
+            if (ctx->row_idx[dir][i * ctx->msize + j].idx == k)
+            {
+              if (last_size == 0)
+              {
+                last_rowitem = i * ctx->msize + j;
+                last_size = ctx->row_idx[dir][last_rowitem].size;
+              }
+              else
+              {
+                if (!(fabs(last_size - ctx->row_idx[dir][i * ctx->msize + j].size) < DBL_EPSILON))
+                {
+                  if (j == ctx->row_size[dir][i] - 1 && j != 0)
+                  {
+                    // Adjust previous
+                    ctx->row_idx[dir][i * ctx->msize + j - 1].size -=
+                        last_size - ctx->row_idx[dir][i * ctx->msize + j].size;
+                    ctx->row_idx[dir][i * ctx->msize + j].size = last_size;
+                  }
+                  else
+                  {
+                    // Adjust next
+                    ctx->row_idx[dir][i * ctx->msize + j + 1].size -=
+                        last_size - ctx->row_idx[dir][i * ctx->msize + j].size;
+                    ctx->row_idx[dir][i * ctx->msize + j].size = last_size;
+                  }
+                  adjusted = 1;
+                }
+              }
+              if (ctx->item[k].dir[dir].calc_size < ctx->row_idx[dir][i * ctx->msize + j].size)
+                ctx->item[k].dir[dir].calc_size = ctx->row_idx[dir][i * ctx->msize + j].size;
+            }
+          }
+        }
       }
     }
   }
-  
-  for (int i = 0; i < ctx->row_cnt[dir]; i++) {
+
+  for (int i = 0; i < ctx->row_cnt[dir]; i++)
+  {
     double coord = 0;
-    for (int j = 0; j < ctx->row_size[dir][i]; j++) {
-      if ( !ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled) {
-	ctx->row_idx[dir][i * ctx->msize + j].coord = coord;
-	if (coord > ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].calc_coord)
-	  ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].calc_coord = coord;
-	coord += ctx->row_idx[dir][i * ctx->msize + j].size;
+    for (int j = 0; j < ctx->row_size[dir][i]; j++)
+    {
+      if (!ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].disabled)
+      {
+        ctx->row_idx[dir][i * ctx->msize + j].coord = coord;
+        if (coord > ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].calc_coord)
+          ctx->item[ctx->row_idx[dir][i * ctx->msize + j].idx].dir[dir].calc_coord = coord;
+        coord += ctx->row_idx[dir][i * ctx->msize + j].size;
       }
     }
   }
@@ -273,31 +313,30 @@ static void calculate(sCtx *ctx, int dir)
 #endif
 }
 
-
-int graph_layout(Graph* graph, int mod_size, float wind_width, float wind_height, 
-    ccm_tInt* prio, 
-    ccm_tInt* top_neighbour, ccm_tInt* down_neighbour, ccm_tInt* left_neighbour,
-    ccm_tInt* right_neighbour, ccm_tFloat* pref_width, ccm_tFloat* pref_height, 
-    ccm_tInt* fix_width, ccm_tInt* fix_height, ccm_tFloat *calc_x, ccm_tFloat* calc_y, 
-    ccm_tFloat* calc_width, ccm_tFloat* calc_height,
-    float* calc_wind_width, float* calc_wind_height)
+int graph_layout(Graph* graph, int mod_size, float wind_width, float wind_height, ccm_tInt* prio,
+                 ccm_tInt* top_neighbour, ccm_tInt* down_neighbour, ccm_tInt* left_neighbour,
+                 ccm_tInt* right_neighbour, ccm_tFloat* pref_width, ccm_tFloat* pref_height,
+                 ccm_tInt* fix_width, ccm_tInt* fix_height, ccm_tFloat* calc_x, ccm_tFloat* calc_y,
+                 ccm_tFloat* calc_width, ccm_tFloat* calc_height, float* calc_wind_width,
+                 float* calc_wind_height)
 {
   int i;
-  sCtx *ctx;
+  sCtx* ctx;
 
   // Create context
-  ctx = (sCtx *)calloc(1, sizeof(sCtx));
-  ctx->row_idx[0] = (sRowItem *)calloc(mod_size, ROW_MAX * sizeof(sRowItem));
-  ctx->row_idx[1] = (sRowItem *)calloc(mod_size, ROW_MAX * sizeof(sRowItem));
-  ctx->row_size[0] = (int *)calloc(ROW_MAX, sizeof(int));
-  ctx->row_size[1] = (int *)calloc(ROW_MAX, sizeof(int));
-  ctx->item = (sItem *)calloc(mod_size, sizeof(sItem));
+  ctx = (sCtx*)calloc(1, sizeof(sCtx));
+  ctx->row_idx[0] = (sRowItem*)calloc(mod_size, ROW_MAX * sizeof(sRowItem));
+  ctx->row_idx[1] = (sRowItem*)calloc(mod_size, ROW_MAX * sizeof(sRowItem));
+  ctx->row_size[0] = (int*)calloc(ROW_MAX, sizeof(int));
+  ctx->row_size[1] = (int*)calloc(ROW_MAX, sizeof(int));
+  ctx->item = (sItem*)calloc(mod_size, sizeof(sItem));
 
   // Insert data into context
   ctx->msize = mod_size;
   ctx->wind_size[VERT] = wind_height;
   ctx->wind_size[HORIZ] = wind_width;
-  for (i = 0; i < ctx->msize; i++) {
+  for (i = 0; i < ctx->msize; i++)
+  {
     ctx->item[i].dir[VERT].pref_size = pref_height[i];
     ctx->item[i].dir[VERT].fix_size = fix_height[i];
     ctx->item[i].dir[HORIZ].pref_size = pref_width[i];
@@ -306,19 +345,23 @@ int graph_layout(Graph* graph, int mod_size, float wind_width, float wind_height
   }
 
   // Create rows
-  for (i = 0; i < ctx->msize; i++) {
-    if (top_neighbour[i] == BORDER) {
+  for (i = 0; i < ctx->msize; i++)
+  {
+    if (top_neighbour[i] == BORDER)
+    {
       ctx->row_cnt[VERT]++;
       next(ctx, VERT, i, ctx->row_cnt[VERT] - 1, down_neighbour, top_neighbour);
     }
-    if (left_neighbour[i] == BORDER) {
+    if (left_neighbour[i] == BORDER)
+    {
       ctx->row_cnt[HORIZ]++;
       next(ctx, HORIZ, i, ctx->row_cnt[HORIZ] - 1, right_neighbour, left_neighbour);
     }
   }
 
   // Disable
-  if (graph->mode == graph_eMode_Runtime) {  
+  if (graph->mode == graph_eMode_Runtime)
+  {
     disable(ctx, VERT);
     disable(ctx, HORIZ);
   }
@@ -341,7 +384,8 @@ int graph_layout(Graph* graph, int mod_size, float wind_width, float wind_height
 #endif
 
   // Result
-  for (i = 0; i < ctx->msize; i++) {
+  for (i = 0; i < ctx->msize; i++)
+  {
     calc_x[i] = ctx->item[i].dir[HORIZ].calc_coord;
     calc_y[i] = ctx->item[i].dir[VERT].calc_coord;
     calc_width[i] = ctx->item[i].dir[HORIZ].calc_size;
