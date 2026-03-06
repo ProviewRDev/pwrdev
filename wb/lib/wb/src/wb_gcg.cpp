siamect/proview

commit d29602e64637b02003e85259cb0ce0667bc31335

rt iocopyarea for ATv, DTv and Sv, and time and string locks

wb/lib/wb/src/wb_gcg.cpp
@@ -3619,20 +3619,14 @@ static int	gcg_get_outputstring_spec(
     free((char *) attrref);
     return GSX__SPECFOUND;
   }
-  case pwr_cClass_GetSp:
-  case pwr_cClass_GetATp:
-  case pwr_cClass_GetDTp:
   case pwr_cClass_GetDataRefp: {
     /**********************************************************
-     *  GetSp, GetATP, GetDTp, GetDataRefp
+     *  GetDataRefp
      ***********************************************************/	
 
     pwr_tAName aname;
 
     switch ( output_node->ln.cid) {
-    case pwr_cClass_GetSp: strcpy( aname, "SpObject"); break;
-    case pwr_cClass_GetATp: strcpy( aname, "ATpObject"); break;
-    case pwr_cClass_GetDTp: strcpy( aname, "DTpObject"); break;
     case pwr_cClass_GetDataRefp: strcpy( aname, "DataRefpObject"); break;
     default: ;
     }
@@ -6731,6 +6725,7 @@ int	gcg_comp_m8( gcg_ctx gcgctx, vldh_t_node node)
 	pwr_tClassId		cid;
 	pwr_tDisableAttr	disabled;
 	pwr_tCid		scid;
+	char			*name;
 
 	ldhses = (node->hn.wind)->hw.ldhses;  
 	
@@ -6888,29 +6883,19 @@ int	gcg_comp_m8( gcg_ctx gcgctx, vldh_t_node node)
 	    gcg_error_msg( gcgctx, GSX__REFCLASS, node);  
 	    return GSX__NEXTNODE;
 	  }
-
-	  /* Insert io object in ioread list */
-	  gcg_aref_insert( gcgctx, attrref, GCG_PREFIX_REF, node);
-	  return GSX__SUCCESS;
 	  break;
 	case pwr_cClass_GetATv:
 	  if ( cid != pwr_cClass_ATv) {
 	    gcg_error_msg( gcgctx, GSX__REFCLASS, node);  
 	    return GSX__NEXTNODE;
 	  }
-
-	  /* Insert io object in ioread list */
-	  gcg_aref_insert( gcgctx, attrref, GCG_PREFIX_REF, node);
-	  return GSX__SUCCESS;
+	  break;
 	case pwr_cClass_GetDTv:
 	  if ( cid != pwr_cClass_DTv) {
 	    gcg_error_msg( gcgctx, GSX__REFCLASS, node);  
 	    return GSX__NEXTNODE;
 	  }
-
-	  /* Insert io object in ioread list */
-	  gcg_aref_insert( gcgctx, attrref, GCG_PREFIX_REF, node);
-	  return GSX__SUCCESS;
+	  break;
 	case pwr_cClass_GetConstAv:
 	  if ( cid != pwr_cClass_ConstAv) {
 	    gcg_error_msg( gcgctx, GSX__REFCLASS, node);  
@@ -6947,6 +6932,18 @@ int	gcg_comp_m8( gcg_ctx gcgctx, vldh_t_node node)
 
 	/* Insert io object in ioread list */
 	gcg_ioread_insert( gcgctx, attrref, GCG_PREFIX_REF);
+
+	/* Print init call */
+	switch ( node->ln.cid) {
+	case pwr_cClass_GetATv:
+	case pwr_cClass_GetDTv:
+	case pwr_cClass_GetSv:
+	  sts = gcg_get_structname( gcgctx, node->ln.oid, &name);
+	  IF_PR fprintf( gcgctx->files[GCGM1_REF_FILE], 
+			 "%s_init( tp);\n",
+			 name);
+	default: ;
+	}
 
 	return GSX__SUCCESS;
 }
@@ -7034,7 +7031,7 @@ int	gcg_comp_m9( gcg_ctx gcgctx, vldh_t_node node)
 * vldh_t_node	node		I	vldh node.
 *
 * Description:
-*	Compile method for GetDp, GetAp, GetSp, GetATp, GetDTp, GetDataRefp.
+*	Compile method for GetDp, GetAp, GetSp, GetDataRefp.
 *	Checks that the referenced object exists and that the referenced
 *	parameter exists in that object, and that the type of the parameter
 *	is correct.
@@ -8315,10 +8312,7 @@ int	gcg_comp_m11( gcg_ctx gcgctx, vldh_t_node node)
 	    gcg_error_msg( gcgctx, GSX__DISABLED, node);
 	}
 
-        if ( cid == pwr_cClass_Sv ||
-	     cid == pwr_cClass_ATv ||
-	     cid == pwr_cClass_DTv ||
-	     cid == pwr_cClass_DataRefv) {
+        if ( cid == pwr_cClass_DataRefv) {
 	  /* Insert io object in ref list */
 	  gcg_aref_insert( gcgctx, refattrref, GCG_PREFIX_REF, node);
 
@@ -11043,6 +11037,24 @@ int	gcg_comp_m36( gcg_ctx gcgctx, vldh_t_node node)
 	      return GSX__NEXTNODE;
 	    }
 	    break;
+	  case pwr_eType_Time:
+	    if ( node->ln.cid != pwr_cClass_GetATp) {
+	      gcg_error_msg( gcgctx, GSX__REFPARTYPE, node);  
+	      return GSX__NEXTNODE;
+	    }
+	    break;
+	  case pwr_eType_DeltaTime:
+	    if ( node->ln.cid != pwr_cClass_GetDTp) {
+	      gcg_error_msg( gcgctx, GSX__REFPARTYPE, node);  
+	      return GSX__NEXTNODE;
+	    }
+	    break;
+	  case pwr_eType_String:
+	    if ( node->ln.cid != pwr_cClass_GetSp) {
+	      gcg_error_msg( gcgctx, GSX__REFPARTYPE, node);  
+	      return GSX__NEXTNODE;
+	    }
+	    break;
 	  default:
 	    /* Not allowed type */
 	    gcg_error_msg( gcgctx, GSX__REFPARTYPE, node);  
@@ -11062,6 +11074,7 @@ int	gcg_comp_m36( gcg_ctx gcgctx, vldh_t_node node)
 
 	/* Insert object in ref list */
 	gcg_aref_insert( gcgctx, refattrref, GCG_PREFIX_REF, node);
+
 
 	return GSX__SUCCESS;
 }
@@ -13880,6 +13893,15 @@ int	gcg_comp_m55( gcg_ctx gcgctx, vldh_t_node node)
 	gcg_scantime_print( gcgctx, node->ln.oid);
 	gcg_timer_print( gcgctx, node->ln.oid);
 	
+	/* Print init call */
+	switch ( node->ln.cid) {
+	case pwr_cClass_CurrentTime:
+	  IF_PR fprintf( gcgctx->files[GCGM1_REF_FILE], 
+			 "%s_init( tp);\n",
+			 name);
+	default: ;
+	}
+
 	return GSX__SUCCESS;
 }
 /*************************************************************************

nmps/lib/nmps/src/nmps_appl.c
@@ -57,7 +57,7 @@
 #include "co_math.h"
 #include "rt_gdh.h"
 #include "rt_errh.h"
-#include "rt_nmps_lock.h"
+#include "rt_lck.h"
 #include "rt_gdh_msg.h"
 #include "rt_hash_msg.h"
 #include "rs_nmps_msg.h"
@@ -410,7 +410,7 @@ nmpsappl_MirrorInit(
 		(nmpsappl_t_datainfo *) calloc( applctx->total_cellsize, 
 		sizeof(nmpsappl_t_datainfo));
 
-	nmps_create_lock( &sts);
+	lck_Create( &sts, lck_eLock_NMps);
 	if ( EVEN(sts)) return sts;
 
 	applctx->options = options;
@@ -505,10 +505,10 @@ nmpsappl_Mirror(
 	for ( i = 0; i < applctx->cellist_count; i++)
 	{
 	  cellist_ptr = applctx->cellist[i];
-	  nmps_Lock;
+	  lck_LockNMps;
 	  memcpy( cellist_ptr->tmp_cell, cellist_ptr->object_ptr,
 			cellist_ptr->tmp_size);
-	  nmps_Unlock;
+	  lck_UnlockNMps;
 	}
 
 	applctx->data_count = 0;

nmps/lib/nmps/src/nmps_plc.c
@@ -47,7 +47,7 @@
 #include "co_math.h"
 #include "rt_gdh.h"
 #include "rt_plc.h"
-#include "rt_nmps_lock.h"
+#include "rt_lck.h"
 #include "nmps.h"
 #include "rs_nmps_msg.h"
 
@@ -101,7 +101,7 @@ void NMpsCell_init( pwr_sClass_NMpsCell  *object)
 	int		i;
 	pwr_tStatus 	sts;
 
-	nmps_create_lock( &sts);
+	lck_Create( &sts, lck_eLock_NMps);
 
 	if ( !(object->Function & NMPS_CELLFUNC_BACKUP))
 	  NMpsCell_init_time( object);
@@ -149,13 +149,13 @@ void NMpsCell_exec(
 	  if ( !(object->ReloadDone & NMPS_CELL_RELOADDONE))
 	    return;
 	  else {
-	    nmps_Lock;
+	    lck_LockNMps;
 	    NMpsCell_init_time( object);
 	    object->ReloadDone &= ~NMPS_CELL_RELOADDONE;
 	  }
 	}
 	else
-	  nmps_Lock;
+	  lck_LockNMps;
 
 
 	if ( object->FrontNew) object->FrontNew = 0;
@@ -434,7 +434,7 @@ void NMpsCell_exec(
 	    case NMPS_OPTYPE_FORWARD_FRONT:
 	      /* Insert at front of object front of data */
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      if ( object->LastIndex > 0)
@@ -472,7 +472,7 @@ void NMpsCell_exec(
 	      {
 	        /* This it not the correct object */
 	        object->InFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 	        return;
 	      }
 	      object->Data1_Back = 1;
@@ -485,7 +485,7 @@ void NMpsCell_exec(
 	      break;
 	    case NMPS_OPTYPE_FORWARD_UNIT:
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      if ( object->LastIndex > 0)
@@ -520,7 +520,7 @@ void NMpsCell_exec(
 	    case NMPS_OPTYPE_REVERSE_BACK:
 	      /* Insert at rear of object back of data */
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      data_last = (plc_t_DataInfo *) &object->Data1P;
@@ -541,7 +541,7 @@ void NMpsCell_exec(
 	    case NMPS_OPTYPE_REVERSE_UNIT:
 	      /* Insert at rear of object all of data */
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      data_last = (plc_t_DataInfo *) &object->Data1P;
@@ -567,7 +567,7 @@ void NMpsCell_exec(
 	      {
 	        /* This it not the correct object */
 	        object->InFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 	        return;
 	      }
 	      data_last->Data_Front = 1;
@@ -591,7 +591,7 @@ void NMpsCell_exec(
 	      if ( object->LastIndex == 0)
 	      {
 	        object->OutRearFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 
@@ -637,7 +637,7 @@ void NMpsCell_exec(
 	      if ( object->LastIndex == 0)
 	      {
 	        object->OutFrontFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 #if defined OS_LINUX
@@ -683,7 +683,7 @@ void NMpsCell_exec(
 	}
 	object->NumberOfData = object->LastIndex;
 
-	nmps_Unlock;
+	lck_UnlockNMps;
 }
 
 
@@ -695,7 +695,7 @@ void NMpsStoreCell_init( pwr_sClass_NMpsStoreCell  *object)
 	int		i;
 	pwr_tStatus 	sts;
 
-	nmps_create_lock( &sts);
+	lck_Create( &sts, lck_eLock_NMps);
 
 	if ( !(object->Function & NMPS_CELLFUNC_BACKUP))
 	  NMpsCell_init_time( (pwr_sClass_NMpsCell *) object);
@@ -747,13 +747,13 @@ void NMpsStoreCell_exec(
 	    return;
 	  else
 	  {
-	    nmps_Lock;
+	    lck_LockNMps;
 	    NMpsCell_init_time( (pwr_sClass_NMpsCell *)object);
 	    object->ReloadDone &= ~NMPS_CELL_RELOADDONE;
 	  }
 	}
 	else
-	  nmps_Lock;
+	  lck_LockNMps;
 
 	if ( object->FrontNew) object->FrontNew = 0;
 	if ( object->RearNew) object->RearNew = 0;
@@ -1195,7 +1195,7 @@ void NMpsStoreCell_exec(
 	    case NMPS_OPTYPE_FORWARD_FRONT:
 	      /* Insert at front of object front of data */
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      if ( object->LastIndex > 0)
@@ -1232,7 +1232,7 @@ void NMpsStoreCell_exec(
 	      {
 	        /* This it not the correct object */
 	        object->InFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 	        return;
 	      }
 	      object->Data1_Back = 1;
@@ -1245,7 +1245,7 @@ void NMpsStoreCell_exec(
 	      break;
 	    case NMPS_OPTYPE_FORWARD_UNIT:
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      if ( object->LastIndex > 0)
@@ -1279,7 +1279,7 @@ void NMpsStoreCell_exec(
 	    case NMPS_OPTYPE_REVERSE_BACK:
 	      /* Insert at rear of object back of data */
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      data_last = (plc_t_DataInfo *) &object->Data1P;
@@ -1297,7 +1297,7 @@ void NMpsStoreCell_exec(
 	    case NMPS_OPTYPE_REVERSE_UNIT:
 	      /* Insert at rear of object all of data */
 	      if ( object->CellFull) {
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 	      data_last = (plc_t_DataInfo *) &object->Data1P;
@@ -1320,7 +1320,7 @@ void NMpsStoreCell_exec(
 	      {
 	        /* This it not the correct object */
 	        object->InFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 	        return;
 	      }
 	      data_last->Data_Front = 1;
@@ -1341,7 +1341,7 @@ void NMpsStoreCell_exec(
 	      if ( object->LastIndex == 0)
 	      {
 	        object->OutRearFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 
@@ -1411,7 +1411,7 @@ void NMpsStoreCell_exec(
 	      if ( object->LastIndex == 0)
 	      {
 	        object->OutFrontFlag = 0;
-		nmps_Unlock;
+		lck_UnlockNMps;
 		return;
 	      }
 
@@ -1481,7 +1481,7 @@ void NMpsStoreCell_exec(
 	}
 	object->NumberOfData = object->DataSelected;
 
-	nmps_Unlock;
+	lck_UnlockNMps;
 }
 
 /*_*

src/exe/rt_ini/src/rt_ini.c
@@ -927,9 +927,15 @@ load_backup ()
   pwr_sClass_AvArea		*avp;
   pwr_sClass_DvArea		*dvp;
   pwr_sClass_IvArea		*ivp;
+  pwr_sClass_ATvArea		*atvp;
+  pwr_sClass_DTvArea		*dtvp;
+  pwr_sClass_SvArea		*svp;
   pwr_sClass_InitArea		*iavp;
   pwr_sClass_InitArea		*idvp;
   pwr_sClass_InitArea		*iivp;
+  pwr_sClass_InitArea		*iatvp;
+  pwr_sClass_InitArea		*idtvp;
+  pwr_sClass_InitArea		*isvp;
   pwr_tStatus			sts;
   int				i;
   pwr_sClass_IOHandler		*iop;
@@ -1001,6 +1007,42 @@ load_backup ()
     return;
   }
 
+  sts = gdh_NameToObjid("pwrNode-active-io-atv", &oid);
+  if (EVEN(sts)) {
+    errh_Error("gdh_NameToObjid(pwrNode-active-io-atv, &oid), %m", sts);
+    return;
+  }
+
+  sts = gdh_ObjidToPointer(oid, (void *) &atvp);
+  if (EVEN(sts)) {
+    errh_Error("gdh_ObjidToPointer(oid, (void *) &atvp), %m", sts);
+    return;
+  }
+
+  sts = gdh_NameToObjid("pwrNode-active-io-dtv", &oid);
+  if (EVEN(sts)) {
+    errh_Error("gdh_NameToObjid(pwrNode-active-io-dtv, &oid), %m", sts);
+    return;
+  }
+
+  sts = gdh_ObjidToPointer(oid, (void *) &dtvp);
+  if (EVEN(sts)) {
+    errh_Error("gdh_ObjidToPointer(oid, (void *) &dtvp), %m", sts);
+    return;
+  }
+
+  sts = gdh_NameToObjid("pwrNode-active-io-sv", &oid);
+  if (EVEN(sts)) {
+    errh_Error("gdh_NameToObjid(pwrNode-active-io-sv, &oid), %m", sts);
+    return;
+  }
+
+  sts = gdh_ObjidToPointer(oid, (void *) &svp);
+  if (EVEN(sts)) {
+    errh_Error("gdh_ObjidToPointer(oid, (void *) &svp), %m", sts);
+    return;
+  }
+
   sts = gdh_NameToObjid("pwrNode-active-io-iv_init", &oid);
   if (EVEN(sts)) {
     errh_Error("gdh_NameToObjid(pwrNode-active-io-iv_init, &oid), %m", sts);
@@ -1010,6 +1052,42 @@ load_backup ()
   sts = gdh_ObjidToPointer(oid, (void *) &iivp);
   if (EVEN(sts)) {
     errh_Error("gdh_ObjidToPointer(oid, (void *) &iivp), %m", sts);
+    return;
+  }
+
+  sts = gdh_NameToObjid("pwrNode-active-io-atv_init", &oid);
+  if (EVEN(sts)) {
+    errh_Error("gdh_NameToObjid(pwrNode-active-io-atv_init, &oid), %m", sts);
+    return;
+  }
+
+  sts = gdh_ObjidToPointer(oid, (void *) &iatvp);
+  if (EVEN(sts)) {
+    errh_Error("gdh_ObjidToPointer(oid, (void *) &iatvp), %m", sts);
+    return;
+  }
+
+  sts = gdh_NameToObjid("pwrNode-active-io-dtv_init", &oid);
+  if (EVEN(sts)) {
+    errh_Error("gdh_NameToObjid(pwrNode-active-io-dtv_init, &oid), %m", sts);
+    return;
+  }
+
+  sts = gdh_ObjidToPointer(oid, (void *) &idtvp);
+  if (EVEN(sts)) {
+    errh_Error("gdh_ObjidToPointer(oid, (void *) &idtvp), %m", sts);
+    return;
+  }
+
+  sts = gdh_NameToObjid("pwrNode-active-io-sv_init", &oid);
+  if (EVEN(sts)) {
+    errh_Error("gdh_NameToObjid(pwrNode-active-io-sv_init, &oid), %m", sts);
+    return;
+  }
+
+  sts = gdh_ObjidToPointer(oid, (void *) &isvp);
+  if (EVEN(sts)) {
+    errh_Error("gdh_ObjidToPointer(oid, (void *) &isvp), %m", sts);
     return;
   }
 
@@ -1028,11 +1106,29 @@ load_backup ()
     ivp->Value[i] = *iip;
   }
 
+  for (i = 1; i < iop->ATvCount; i++) {
+    pwr_tTime *iatp = gdh_TranslateRtdbPointer(iatvp->Value[i]);
+    atvp->Value[i] = *iatp;
+  }
+
+  for (i = 0; i < iop->DTvCount; i++) {
+    pwr_tDeltaTime *idtp = gdh_TranslateRtdbPointer(idtvp->Value[i]);
+    dtvp->Value[i] = *idtp;
+  }
+
+  for (i = 0; i < iop->SvCount; i++) {
+    char *istrp = gdh_TranslateRtdbPointer(isvp->Value[i]);
+    strncpy( svp->Value[i], istrp, sizeof(svp->Value[0]));
+  }
+
   typedef struct {
     union {
       pwr_tFloat32 *f;
       pwr_tInt32   *i;
       pwr_tBoolean *b;
+      pwr_tTime    *at;
+      pwr_tDeltaTime *dt;
+      pwr_tString80 *str;
     } actval_p;
     pwr_tUInt32  validx;
     union {
@@ -1046,6 +1142,9 @@ load_backup ()
       pwr_sClass_Di *di;
       pwr_sClass_Do *dox;
       pwr_sClass_Co *co;
+      pwr_sClass_ATv *atv;
+      pwr_sClass_DTv *dtv;
+      pwr_sClass_Sv *sv;
     } op;
   } ini_sRestoreSig;
 
@@ -1101,6 +1200,60 @@ load_backup ()
 
     rsiv[i].actval_p.i = rsiv[i].op.iv->ActualValue;
     rsiv[i].validx = rsiv[i].op.iv->ValueIndex;
+    i++;
+  }
+  
+  ini_sRestoreSig *rsatv = calloc( sizeof(ini_sRestoreSig), iop->ATvCount);
+  i = 1;
+  for ( sts = gdh_GetClassListAttrRef(pwr_cClass_ATv, &aref);
+	ODD(sts);
+	sts = gdh_GetNextAttrRef(pwr_cClass_ATv, &aref, &aref)) {
+    if ( i >= iop->ATvCount) break;
+
+    sts = gdh_AttrRefToPointer( &aref, (pwr_tAddress *)&rsatv[i].op.atv);
+    if ( EVEN(sts)) {
+      errh_Error("gdh_AttrRefToPointer ATv, %m", sts);
+      return;
+    }
+
+    rsatv[i].actval_p.at = rsatv[i].op.atv->ActualValue;
+    rsatv[i].validx = rsatv[i].op.atv->ValueIndex;
+    i++;
+  }
+  
+  ini_sRestoreSig *rsdtv = calloc( sizeof(ini_sRestoreSig), iop->DTvCount);
+  i = 0;
+  for ( sts = gdh_GetClassListAttrRef(pwr_cClass_DTv, &aref);
+	ODD(sts);
+	sts = gdh_GetNextAttrRef(pwr_cClass_DTv, &aref, &aref)) {
+    if ( i >= iop->DTvCount) break;
+
+    sts = gdh_AttrRefToPointer( &aref, (pwr_tAddress *)&rsdtv[i].op.dtv);
+    if ( EVEN(sts)) {
+      errh_Error("gdh_AttrRefToPointer DTv, %m", sts);
+      return;
+    }
+
+    rsdtv[i].actval_p.dt = rsdtv[i].op.dtv->ActualValue;
+    rsdtv[i].validx = rsdtv[i].op.dtv->ValueIndex;
+    i++;
+  }
+  
+  ini_sRestoreSig *rssv = calloc( sizeof(ini_sRestoreSig), iop->SvCount);
+  i = 0;
+  for ( sts = gdh_GetClassListAttrRef(pwr_cClass_Sv, &aref);
+	ODD(sts);
+	sts = gdh_GetNextAttrRef(pwr_cClass_Sv, &aref, &aref)) {
+    if ( i >= iop->SvCount) break;
+
+    sts = gdh_AttrRefToPointer( &aref, (pwr_tAddress *)&rssv[i].op.sv);
+    if ( EVEN(sts)) {
+      errh_Error("gdh_AttrRefToPointer Sv, %m", sts);
+      return;
+    }
+
+    rssv[i].actval_p.str = rssv[i].op.sv->ActualValue;
+    rssv[i].validx = rssv[i].op.sv->ValueIndex;
     i++;
   }
   
@@ -1258,6 +1411,24 @@ load_backup ()
   }
   free( rsiv);
 
+  for ( i = 1; i < iop->ATvCount; i++) {
+    rsatv[i].op.atv->ActualValue = rsatv[i].actval_p.at;
+    rsatv[i].op.atv->ValueIndex = rsatv[i].validx;
+  }
+  free( rsatv);
+
+  for ( i = 0; i < iop->DTvCount; i++) {
+    rsdtv[i].op.dtv->ActualValue = rsdtv[i].actval_p.dt;
+    rsdtv[i].op.dtv->ValueIndex = rsdtv[i].validx;
+  }
+  free( rsdtv);
+
+  for ( i = 0; i < iop->SvCount; i++) {
+    rssv[i].op.sv->ActualValue = rssv[i].actval_p.str;
+    rssv[i].op.sv->ValueIndex = rssv[i].validx;
+  }
+  free( rssv);
+
   for ( i = 0; i < iop->AiCount; i++) {
     rsai[i].op.ai->ActualValue = rsai[i].actval_p.f;
     rsai[i].op.ai->ValueIndex = rsai[i].validx;
@@ -1316,6 +1487,21 @@ load_backup ()
   for (i = 0; i < iop->IvCount; i++) {
     pwr_tInt32 *iip = gdh_TranslateRtdbPointer(iivp->Value[i]);
     *iip = ivp->Value[i];
+  }
+
+  for (i = 1; i < iop->ATvCount; i++) {
+    pwr_tTime *iatp = gdh_TranslateRtdbPointer(iatvp->Value[i]);
+    *iatp = atvp->Value[i];
+  }
+
+  for (i = 0; i < iop->DTvCount; i++) {
+    pwr_tDeltaTime *idtp = gdh_TranslateRtdbPointer(idtvp->Value[i]);
+    *idtp = dtvp->Value[i];
+  }
+
+  for (i = 0; i < iop->SvCount; i++) {
+    char *istrp = gdh_TranslateRtdbPointer(isvp->Value[i]);
+    strncpy( istrp, svp->Value[i], 80);
   }
 }
 

src/exp/rt/src/rt_plc_process.c
@@ -68,7 +68,7 @@
 #include "rt_que.h"
 #include "rt_csup.h"
 #include "rt_ini_event.h"
-#include "rt_nmps_lock.h"
+#include "rt_lck.h"
 #include "rt_aproc.h"
 #include "rt_pwr_msg.h"
 
@@ -208,7 +208,9 @@ int main (
 
     stop_threads(pp);
     clean_all(pp);
-    nmps_delete_lock( &sts);
+    lck_Delete( &sts, lck_eLock_NMps);
+    lck_Delete( &sts, lck_eLock_Time);
+    lck_Delete( &sts, lck_eLock_Str);
     break;
   case ini_mEvent_oldPlcStop:
     errh_SetStatus( PWR__SRVTERM);
@@ -258,12 +260,15 @@ init_process ( char *name)
     exit(sts);
   }
 
-  // nmps_create_lock( &sts);
-  // if (EVEN(sts)) {
-  //   errh_Fatal("nmps_create_lock, %m", sts);
-  //  errh_SetStatus( PWR__SRVTERM);
-  //  exit(sts);
-  //}
+  lck_Create( &sts, lck_eLock_Time);
+  if (EVEN(sts))
+    errh_Fatal("lock create time, %m", sts);
+  lck_Create( &sts, lck_eLock_Str);
+  if (EVEN(sts))
+    errh_Fatal("lock create str, %m", sts);
+  lck_Create( &sts, lck_eLock_NMps);
+  if (EVEN(sts))
+    errh_Fatal("lock create NMps, %m", sts);
 
   if ( strstr( name, "rt_plc_core") != 0)
     pp->is_core = 1;
@@ -427,6 +432,7 @@ init_plc (
 
   init_grafcet(pp);
   link_io_base_areas(pp);
+  pp->system_time = (pwr_tTime *)pp->base.atv_a.p;
 
   return sts;
 }
@@ -555,6 +561,9 @@ link_io_base_areas (
   dlink_area((plc_sDlink *)&pp->base.ii_a, "pwrNode-active-io-ii", pp->IOHandler->IiCount * sizeof(pwr_tInt32));
   dlink_area((plc_sDlink *)&pp->base.io_a, "pwrNode-active-io-io", pp->IOHandler->IoCount * sizeof(pwr_tInt32));
   dlink_area((plc_sDlink *)&pp->base.iv_a, "pwrNode-active-io-iv", pp->IOHandler->IvCount * sizeof(pwr_tInt32));
+  dlink_area((plc_sDlink *)&pp->base.atv_a, "pwrNode-active-io-atv", pp->IOHandler->ATvCount * sizeof(pwr_tTime));
+  dlink_area((plc_sDlink *)&pp->base.dtv_a, "pwrNode-active-io-dtv", pp->IOHandler->DTvCount * sizeof(pwr_tDeltaTime));
+  dlink_area((plc_sDlink *)&pp->base.sv_a, "pwrNode-active-io-sv", pp->IOHandler->SvCount * sizeof(pwr_tString80));
   dlink_area((plc_sDlink *)&pp->base.bi_a, "pwrNode-active-io-bi", pp->IOHandler->BiSize);
   dlink_area((plc_sDlink *)&pp->base.bo_a, "pwrNode-active-io-bo", pp->IOHandler->BoSize);
   dlink_area((plc_sDlink *)&pp->base.av_i, "pwrNode-active-io-av_init", pp->IOHandler->AvCount * sizeof(pwr_tUInt64));
@@ -566,6 +575,9 @@ link_io_base_areas (
   dlink_area((plc_sDlink *)&pp->base.do_i, "pwrNode-active-io-do_init", pp->IOHandler->DoCount * sizeof(pwr_tUInt64));
   dlink_area((plc_sDlink *)&pp->base.ii_i, "pwrNode-active-io-ii_init", pp->IOHandler->IiCount * sizeof(pwr_tUInt64));
   dlink_area((plc_sDlink *)&pp->base.io_i, "pwrNode-active-io-io_init", pp->IOHandler->IoCount * sizeof(pwr_tUInt64));
+  dlink_area((plc_sDlink *)&pp->base.atv_i, "pwrNode-active-io-iatv_init", pp->IOHandler->ATvCount * sizeof(pwr_tTime));
+  dlink_area((plc_sDlink *)&pp->base.dtv_i, "pwrNode-active-io-idtv_init", pp->IOHandler->DTvCount * sizeof(pwr_tDeltaTime));
+  dlink_area((plc_sDlink *)&pp->base.sv_i, "pwrNode-active-io-isv_init", pp->IOHandler->SvCount * sizeof(pwr_tString80));
   dlink_area((plc_sDlink *)&pp->base.bi_i, "pwrNode-active-io-bi_init", pp->IOHandler->BiCount * sizeof(pwr_tUInt64));
   dlink_area((plc_sDlink *)&pp->base.bi_isize, "pwrNode-active-io-bi_initsize", pp->IOHandler->BiCount * sizeof(pwr_tUInt64));
   dlink_area((plc_sDlink *)&pp->base.bo_i, "pwrNode-active-io-bo_init", pp->IOHandler->BoCount * sizeof(pwr_tUInt64));
@@ -616,6 +628,15 @@ link_io_copy_areas (
 
   tp->copy.iv_a = pp->base.iv_a;
   tp->copy.iv_a.p = calloc(1, tp->copy.iv_a.size);
+
+  tp->copy.atv_a = pp->base.atv_a;
+  tp->copy.atv_a.p = calloc(1, tp->copy.atv_a.size);
+
+  tp->copy.dtv_a = pp->base.dtv_a;
+  tp->copy.dtv_a.p = calloc(1, tp->copy.dtv_a.size);
+
+  tp->copy.sv_a = pp->base.sv_a;
+  tp->copy.sv_a.p = calloc(1, tp->copy.sv_a.size);
 
   tp->copy.bi_a = pp->base.bi_a;
   tp->copy.bi_a.p = calloc(1, tp->copy.bi_a.size);

ProviewRDev/pwrdev

commit 5e2fb720e342435038c40fecfec9cb1ce8acc845

Enhance gcg_comp_m37 to support additional connection classes for object retrieval.
Regression from d29602e64637b02003e85259cb0ce0667bc31335 where the changed classes was never added to backup m37 compile.

Closes #66

wb/lib/wb/src/wb_gcg.cpp
@@ -10474,13 +10474,34 @@ int gcg_comp_m37(gcg_ctx gcgctx, vldh_t_node node)
   switch (conn_cid)
   {
   case pwr_cClass_GetIp:
+  case pwr_cClass_GetDTp:
+  case pwr_cClass_GetATp:
+  case pwr_cClass_GetSp:
   {
     // Get the referenced object
     pwr_tAttrRef* attrref;
     pwr_tAttrRef aref;
     char *name_p, *s;
+    char devbody_attr[32];
 
-    sts = ldh_GetObjectPar(ldhses, conn_node->ln.oid, "DevBody", "IpObject", (char**)&attrref, &size);
+    switch (conn_cid)
+    {
+    case pwr_cClass_GetIp:
+      strcpy(devbody_attr, "IpObject");
+      break;
+    case pwr_cClass_GetDTp:
+      strcpy(devbody_attr, "DTpObject");
+      break;
+    case pwr_cClass_GetATp:
+      strcpy(devbody_attr, "ATpObject");
+      break;
+    case pwr_cClass_GetSp:
+      strcpy(devbody_attr, "SpObject");
+      break;
+    default:;
+    }
+
+    sts = ldh_GetObjectPar(ldhses, conn_node->ln.oid, "DevBody", devbody_attr, (char**)&attrref, &size);
     if (EVEN(sts))
       return sts;
 